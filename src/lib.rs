1b192decba1d330529e507a151e93dd9629c9a65
Nikhil034 <nikhilsondarava@lampros.tech>
2025-08-19 13:56:50 +0530

    modified lib.rs - vending machine contract verification

--- src/lib.rs
+++ src/lib.rs
@@ -1,116 +1,79 @@
 //!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```solidity
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment_verifier() public {
-//!         number++;
-//!     }
-//! }
-//! ```
+//! Stylus Cupcake Example
 //!
 //! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
 //! To do this, run `cargo stylus export-abi`.
 //!
 //! Note: this code is a template-only and has not been audited.
 //!
-// Allow `cargo stylus export-abi` to generate a main function.
-#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
-#![cfg_attr(not(any(test, feature = "export-abi")), no_std)]
 
-#[macro_use]
+// Allow `cargo stylus export-abi` to generate a main function if the "export-abi" feature is enabled.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use alloc::vec::Vec;
+use alloy_primitives::{Address, Uint};
 use stylus_cache_sdk::{is_contract_cacheable};
+// Import items from the SDK. The prelude contains common traits and macros.
+use stylus_sdk::alloy_primitives::U256;
+use stylus_sdk::prelude::*;
+use stylus_sdk::{block, console};
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
-
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
+// Define persistent storage using the Solidity ABI.
+// `VendingMachine` will be the entrypoint for the contract.
 sol_storage! {
     #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+    pub struct VendingMachine {
+        // Mapping from user addresses to their cupcake balances.
+        mapping(address => uint256) cupcake_balances;
+        // Mapping from user addresses to the last time they received a cupcake.
+        mapping(address => uint256) cupcake_distribution_times;
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+// Declare that `VendingMachine` is a contract with the following external methods.
 #[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+impl VendingMachine {
+    // Give a cupcake to the specified user if they are eligible (i.e., if at least 5 seconds have passed since their last cupcake).
+    pub fn give_cupcake_to(&mut self, user_address: Address) -> bool {
+        // Get the last distribution time for the user.
+        let last_distribution = self.cupcake_distribution_times.get(user_address);
+        // Calculate the earliest next time the user can receive a cupcake.
+        let five_seconds_from_last_distribution = last_distribution + U256::from(5);
+
+        // Get the current block timestamp.
+        let current_time = block::timestamp();
+        // Check if the user can receive a cupcake.
+        let user_can_receive_cupcake =
+            five_seconds_from_last_distribution <= Uint::<256, 4>::from(current_time);
+
+        if user_can_receive_cupcake {
+            // Increment the user's cupcake balance.
+            let mut balance_accessor = self.cupcake_balances.setter(user_address);
+            let balance = balance_accessor.get() + U256::from(1);
+            balance_accessor.set(balance);
+
+            // Update the distribution time to the current time.
+            let mut time_accessor = self.cupcake_distribution_times.setter(user_address);
+            let new_distribution_time = block::timestamp();
+            time_accessor.set(Uint::<256, 4>::from(new_distribution_time));
+            return true;
+        } else {
+            // User must wait before receiving another cupcake.
+            console!(
+                "HTTP 429: Too Many Cupcakes (you must wait at least 5 seconds between cupcakes)"
+            );
+            return false;
+        }
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-     pub fn is_cacheable(&self) -> bool {
+    pub fn is_cacheable(&self) -> bool {
         is_contract_cacheable()
     }
 
-    /// increment_verifier_verifiers `number` and updates its value in storage.
-    pub fn increment_verifier(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
-    }
 
-    /// Adds the wei value from msg_value to the number in storage.
-    #[payable]
-    pub fn add_from_msg_value(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + self.vm().msg_value());
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_counter() {
-        use stylus_sdk::testing::*;
-        let vm = TestVM::default();
-        let mut contract = Counter::from(&vm);
-
-        assert_eq!(U256::ZERO, contract.number());
-
-        contract.increment_verifier();
-        assert_eq!(U256::from(1), contract.number());
-
-        contract.add_number(U256::from(3));
-        assert_eq!(U256::from(4), contract.number());
-
-        contract.mul_number(U256::from(2));
-        assert_eq!(U256::from(8), contract.number());
-
-        contract.set_number(U256::from(100));
-        assert_eq!(U256::from(100), contract.number());
-
-        // Override the msg value for future contract method invocations.
-        vm.set_value(U256::from(2));
-
-        contract.add_from_msg_value();
-        assert_eq!(U256::from(102), contract.number());
+    // Get the cupcake balance for the specified user.
+    pub fn get_cupcake_balance_for_verifier(&self, user_address: Address) -> Uint<256, 4> {
+        // Return the user's cupcake balance from storage.
+        return self.cupcake_balances.get(user_address);
     }
 }
